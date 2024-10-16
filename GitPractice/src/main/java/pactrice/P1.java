a0e6a245b086f76c4fb8180a12709d6e515382b7
harshika-soni <harshikamuz@gmail.com>
2024-10-16 13:00:21 -0700

    Update P1.java

--- GitPractice/src/main/java/pactrice/P1.java
+++ GitPractice/src/main/java/pactrice/P1.java
@@ -4,6 +4,9 @@ public class P1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("i m m1")	;}
+		int a=10;
+		int b= 20
+         System.out.println(a+b);	
+	}
 
 }
