10d524a8059c96aeb2203d2a1c50eef7526b8f2a
harshika-soni <harshikamuz@gmail.com>
2024-10-16 13:03:49 -0700

    6

--- GitPractice/src/main/java/pactrice/P1.java
+++ GitPractice/src/main/java/pactrice/P1.java
@@ -5,8 +5,9 @@ public class P1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10;
-		int b= 20
-         System.out.println(a+b);	
+		int b= 20;
+         System.out.println(a+b);
+         System.out.println(b-a);
 	}
 
 }
